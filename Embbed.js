87ef89182ae6740e652546d12dffa54deb1d6852
killersparrow0 <84676044+killersparrow0@users.noreply.github.com>
2021-09-05 20:13:28 +0530

    Update Embbed.js

--- Embbed.js
+++ Embbed.js
@@ -1,8 +1,1 @@
-<iframe width="560" height="315" 
-src="https://www.youtube.com/embed/gb
-mlcpmAx_o" title="YouTube video 
-player" frameborder="0" 
-allow="accelerometer; autoplay; 
-clipboard-write; encrypted-media; 
-gyroscope; picture-in-picture" 
-allowfullscreen></iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/gbmlcpmAx_o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
